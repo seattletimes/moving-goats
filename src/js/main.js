655b4e80cf9d0f9ba3b586d1f8328025bcbdfcb9
emilymeng <eeng@seattletimes.com>
2018-10-17 18:49:05 -0700

    publish

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+require("./lib/ads");
+var paywall = require("./lib/paywall");
+setTimeout(() => paywall(10937327), 3000);
+
+var track = require("./lib/tracking");
+
+require("component-responsive-frame/child");
+
+require("./bc-preroll");
